f864848fea2e93110d72be395357cc5d3e322b1e
Nicolas Went <nicolas.went@epita.fr>
2021-01-24 16:19:13 +0100

    [SQL] Created sql cake tables

--- SQL/schema.php
+++ SQL/schema.php
@@ -10,4 +10,20 @@ class LauncherHelperSchema extends CakeSchema {
 	public function after($event = array()) {
 	}
 
+	public $launcherhelper__launcher_images = array(
+	    'id' => array(
+	        'type' => 'integer',
+            'null' => false,
+            'default' => null,
+            'length' => 10,
+            'key' => 'primary'
+        ),
+        'image' => array(
+            'type' => 'string',
+            'null' => false,
+            'default' => null,
+            'collate' => 'latin1_swedish_ci',
+            'charset' => 'latin1'
+        )
+    );
 }
